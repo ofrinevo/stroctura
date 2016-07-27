6363d8277fa252f905ac8f4069e73cadb0d1b005
renenperlman <renenp@gmail.com>
2016-07-27 17:51:37 +0300

    Update sadna.py

--- sadna.py
+++ sadna.py
@@ -1,8 +1,30 @@
-import chimera
-from chimera import runCommand
-import os
+from chimera import runCommand as rc
 
+DEBUG = 0
 
+def get_turn_command(deg,symetry_axis,symetry_center,model_index):
+    command = "turn "
+    command += str(symetry_axis).replace(' ','')[1:-1] #get only x,y,z out of (x,y,z)
+    command += " "
+    command += str(deg) #rotation degree
+    command += " center "
+    command += str(symetry_center).replace(' ','')[1:-1] #get only x,y,z out of (x,y,z)
+    command += " models #"
+    command += str(model_index)
+    return command
 
-print "all done!"
-
+"""
+monomer_file - location of the monomer
+N - number of symmetry
+symetry_axis - a tuple of the form (x,y,z)
+symetry_center -  a tuple of the form (x,y,z)
+"""
+def cyclic_shift(monomer_file,N,symetry_axis,symetry_center):
+    for i in range(N):
+        rc("open " + monomer_file)
+        if i == 0:
+            continue
+        command = get_turn_command(i*360/N,symetry_axis,symetry_center,i)
+        if DEBUG:
+            print command
+        rc(command)
